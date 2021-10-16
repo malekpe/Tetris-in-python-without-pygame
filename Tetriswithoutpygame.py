8961344935a37c8caf4f990d07e7edffa930a859
Malekpe <56176325+malekpe@users.noreply.github.com>
2021-10-17 00:37:06 +0200

    Add files via upload

--- Tetriswithoutpygame.py
+++ Tetriswithoutpygame.py
@@ -3,10 +3,13 @@ from copy import *
 from random import randint
 import time
 from copy import deepcopy
+import winsound
+
 
 class Create_tetraminos:
-    def __init__(self,Tetris):
+    def __init__(self,Tetris, fenWin):
         self.Tetris = Tetris
+        self.fenWin = fenWin
         
         self.pieces = [[[[0,0,0,0],
                         [1,1,1,1],
@@ -182,20 +185,27 @@ class Create_tetraminos:
         self.rotation = 0
         self.random = randint(0,6)
         self.colors = ["blue", "yellow", "pink", "green", "cyan", "red", "orange"]
+        self.Hcolors = list()
+        self.Hcolors.append(self.colors[self.random])
         self.pieceId = 1
         self.tetra = deepcopy(self.pieces[self.rotation][self.random])
         self.tetra_x = 6
         self.tetra_y = 3
+        while True :
+            self.newRandom = randint(0,6)
+            if self.newRandom != self.random:
+                self.futurpiece = self.newRandom
+                break
+        self.pieceApercu = deepcopy(self.pieces[self.rotation][self.futurpiece])
         for j in range(0,4):
             for i in range(0,4):
                 if self.tetra[j][i] == self.random+1:
                     self.tetra[j][i] = self.pieceId
+        self.c_lapercu = Apercu(self, self.Tetris, self.fenWin)
         
     
     def cadriage(self):
-        couleur = 'black'
-        #self.Tetris.create_rectangle(0,0,400,800,fill=couleur)
-        for i in range(0, 400, 40):
+        for i in range(0, 440, 40):
             couleur = 'white'
             self.Tetris.create_rectangle(i, 0, i+2, 800,fill=couleur)
         for i in range(0, 800, 40):
@@ -208,6 +218,9 @@ class Create_tetraminos:
             for j in range(2,22):
                 if self.grid[j][i] != 0 and self.defaite:
                     couleur = 'red'
+                    self.Tetris.create_rectangle((i-3)*40,(j-2)*40,((i-3)+1)*40,((j-2)+1)*40,fill=couleur)
+                elif self.grid[j][i] > 0 and self.grid[j][i] < self.pieceId:
+                    couleur = self.Hcolors[self.grid[j][i]-1]
                     self.Tetris.create_rectangle((i-3)*40,(j-2)*40,((i-3)+1)*40,((j-2)+1)*40,fill=couleur)
                 elif self.grid[j][i] == self.pieceId:
                     couleur = self.colors[self.random]
@@ -316,20 +329,61 @@ class Create_tetraminos:
               
     def rePiece(self):
         self.rotation = 0
+        self.random = self.futurpiece
         while True :
             self.newRandom = randint(0,6)
-            if self.newRandom != self.random:
-                self.random = self.newRandom
+            if self.newRandom != self.futurpiece:
+                self.futurpiece = self.newRandom
                 break
         self.colors = ["blue", "yellow", "pink", "green", "cyan", "red", "orange"]
         self.pieceId += 1
         self.tetra = deepcopy(self.pieces[self.rotation][self.random])
         self.tetra_x = 6
         self.tetra_y = 3
+        self.Hcolors.append(self.colors[self.random])
+        self.pieceApercu = deepcopy(self.pieces[self.rotation][self.futurpiece])
         for j in range(0,4):
             for i in range(0,4):
                 if self.tetra[j][i] == self.random+1:
                     self.tetra[j][i] = self.pieceId
+        self.c_lapercu.re()
+
+class Apercu:
+
+    def __init__(self, tetraminos, Tetris, fenWin):
+        self.fenWin = fenWin
+        self.Apercu = Canvas(self.fenWin,width = 160, height = 160)
+        self.Apercu.pack(side="right")
+        self.Tetris = Tetris
+        self.tetraminos = tetraminos
+        self.grille = deepcopy(self.tetraminos.pieceApercu)
+        self.apDraw()
+
+    def re(self):
+        self.grille = deepcopy(self.tetraminos.pieceApercu)
+        self.apDraw()
+    
+    def apDraw(self):
+        self.Apercu.delete("all")
+
+        for i in range(0,4):
+            for j in range(0,4):
+                if self.grille[j][i] != 0:
+                    couleur = self.tetraminos.colors[self.tetraminos.futurpiece]
+                    self.Apercu.create_rectangle((i)*40,(j)*40,((i)+1)*40,((j)+1)*40,fill=couleur)
+                elif self.grille[j][i] == 0:
+                    couleur = 'black'
+                    self.Apercu.create_rectangle((i)*40,(j)*40,((i)+1)*40,((j)+1)*40,fill=couleur)
+        
+        for i in range(0, 200, 40):
+            couleur = 'white'
+            self.Apercu.create_rectangle(i, 0, i+2, 160,fill=couleur)
+        for i in range(0, 200, 40):
+            couleur = 'white'
+            self.Apercu.create_rectangle(0, i, 160, i+2,fill=couleur)
+
+
+
 
 class Tetris:
 
@@ -338,22 +392,32 @@ class Tetris:
     def run(self):
         fenWin = Tk()
         fenWin.title('Tetris')
-        fenWin.geometry('400x805')
+        fenWin.geometry('600x800')
+
+        self.fenWin = fenWin
 
         self.Tetris = Canvas(fenWin,width = 400, height = 800)
-        self.Tetris.pack()
+        self.Tetris.pack(side="left")
 
-        self.tetraminos = Create_tetraminos(self.Tetris)
+        self.tetraminos = Create_tetraminos(self.Tetris, self.fenWin)
         self.tetraminos.jeu()
-        
+        #self.c_lapercu = Apercu(self.tetraminos, self.Tetris, self.fenWin)
+
         fenWin.bind('<Key>', self.handleKeyboardEvent)
 
+        self.musique = int(time.time())
+        winsound.PlaySound("Tetris.wav", winsound.SND_ASYNC)
+
         while True:
+            if int(time.time() > self.musique+80):
+                self.musique = int(time.time())
+                winsound.PlaySound("Tetris.wav", winsound.SND_ASYNC)
             if time.time()>(self.clock+1*(0.98**(self.tetraminos.pieceId-1))) and not self.tetraminos.defaite:
                 self.clock = time.time()
                 self.tetraminos.autodown()
             if self.tetraminos.defaite:
                 self.tetraminos.draw_game()
+                winsound.PlaySound(None, winsound.SND_ASYNC)
                 if not self.tetraminos.saved:
                     self.tetraminos.saved = True
                     with open("Tetris_historique_parties.txt", "a", encoding="utf_8") as file:
@@ -361,8 +425,6 @@ class Tetris:
                         file.write(f"\nscore : {self.tetraminos.score}\nlignes détruites : {self.tetraminos.lignes_detruite}\npieces jouées : {self.tetraminos.pieceId-1}\ntemps de jeu : {time.localtime(int(time.time())-self.tetraminos.jeuTemps).tm_min}min{time.localtime(int(time.time())-self.tetraminos.jeuTemps).tm_sec}s")
             fenWin.update_idletasks()
             fenWin.update()
-
-
         
     def handleKeyboardEvent(self, event):
 
@@ -377,7 +439,6 @@ class Tetris:
         
         if (event.keysym == "Down") and not self.tetraminos.defaite:
             self.tetraminos.autodown()
-
 
 tetris = Tetris()
 tetris.run()
